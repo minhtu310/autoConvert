d86c15be07687afa6de2b6ffdba384813d90fa29
minhtu310 <minhtu736@live.com>
2021-05-30 11:59:36 +0700

    Hello again

--- atc/views.py
+++ atc/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-def hello_world(request):
-   return render(request, 'hello_world.html', {})
+from django.http import HttpResponse
+def index(request):
+   return HttpResponse("Hello world !!")
 
 # Create your views here.
--- autoConvert/urls.py
+++ autoConvert/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from atc import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello_world.urls')),
+    path('', views.index, name='home'),
 ]
